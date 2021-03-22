1e592dcf0566172b93aee2a29da0d649bab97c73
Melroy Rodrigues <rockstarodmel@gmail.com>
2021-03-22 19:22:27 +0300

    changes

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').parse();
-}
 const express = require('express');
 const indexRouter = require('./routes/index');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.connect("mongodb+srv://Melroy:" + process.env.DATABASE_URL + "@cluster0.pin1z.mongodb.net/ask-the-master?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }
 ).then(() => {
     console.log("Connected to MongoDB")
 }).catch(err => {
