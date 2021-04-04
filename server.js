f89ab1119befb4fd77d5ea65b8580ba3c84f9af6
Melroy Rodrigues <rockstarodmel@gmail.com>
2021-04-04 09:54:25 +0300

    chamge to the mongodb

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true }
-mongoose.connect("mongodb+srv://Melroy:melroy1234@cluster0.pin1z.mongodb.net/ask-the-master?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    // mongoose.connect("mongodb+srv://Melroy:melroy1234@cluster0.pin1z.mongodb.net/ask-the-master?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Connected to MongoDB")
     }).catch(err => {
